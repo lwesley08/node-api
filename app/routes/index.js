e153636d35603f6fb3e8e84069e72869bd3c226a
lwesley08 <artisticliberties0821@gmail.com>
2020-06-22 18:31:16 -0400

    adding default route and prod build

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,4 +2,8 @@ const noteRoutes = require('./note_routes');
 
 module.exports = function(app, db) {
     noteRoutes(app, db);
+    
+    app.get("*", function(req, res) {
+        res.send("App works!!!!!");
+    })
 }
