695e63de3fe0abb8e61c12b4332523e3704774ae
Liberty Wesley <lwesley@foundationsoft.com>
2020-06-30 14:44:15 -0400

    webpack for ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
-const path = require('path');
+var path =  require('path');
 
 module.exports = {
-  entry: './server.js',
+  entry: ["./index.ts"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'api.bundle.js'
+    filename: 'api.bundle.js',
+    path: path.resolve(__dirname,"dist")
   },
-  target: 'node'
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader',exclude: /node_modules/,},
+      { test: /\.json/, loader: "json-loader", exclude:/node_modules/,}
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js",".json"]
+  },
+  target: 'node',
+  node: {
+        __dirname: true
+    },
+  plugins: []
 };
